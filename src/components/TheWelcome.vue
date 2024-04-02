e085c3663eab10a7e47e7cc29ba07148da35cd90
DaffaKhaiza <daffakhaizairawan@student.uir.ac.id>
2024-04-02 17:10:21 +0700

    tugas1

---
+++ src/components/TheWelcome.vue
@@ -0,0 +1,45 @@
+<script setup>
+import WelcomeItem from './WelcomeItem.vue'
+import DocumentationIcon from './icons/IconDocumentation.vue'
+import ToolingIcon from './icons/IconTooling.vue'
+import EcosystemIcon from './icons/IconEcosystem.vue'
+import CommunityIcon from './icons/IconCommunity.vue'
+import SupportIcon from './icons/IconSupport.vue'
+</script>
+
+<template>
+  <WelcomeItem>
+    <template #icon>
+      <DocumentationIcon />
+    </template>
+    <template #heading>Perkenalan</template>
+
+    Nama saya Daffa Khaiza Irawan saya kelahiran tahun 2004 bulan juli tanggal 25.saya menempuh pendidikan SD sampai SMA di kota kelahiran saya yaitu kota Pasir Pengaraian
+    provides you with all information you need to get started.
+  </WelcomeItem>
+
+  <WelcomeItem>
+    <template #icon>
+      <ToolingIcon />
+    </template>
+    <template #heading>Hobby</template>
+Hobby saya sejak sedari kecil sudah jelas bermain game, tetapi sesudah saya memasuki bangku SMP dan SMA saya suka bermain basket karena dlu saya ingat melihat senior bermain basket.dan tentu saya ikut dalam ekskul basket pada saat saya SMA.
+dan tentu saya tidak hanya bermain game console dan pc saya juga ikut tournament besar pada game tersebut saya ingat betul saya ikut ALGS Qualifier APAC Apex Legends 2021.
+  </WelcomeItem>
+
+  <WelcomeItem>
+    <template #icon>
+      <EcosystemIcon />
+    </template>
+    <template #heading>sneakers that i like.</template>
+
+    
+    <a href="https://jdsports.id/product/nike-dunk-low-cacao-wow-women-s-dd1503-124" target="_blank" rel="noopener">Nike Dunk Cacao Wow</a>,
+    <a href="https://www.nike.com/id/launch/t/air-jordan-1-dark-mocha" target="_blank" rel="noopener">Nike Air Jordan Dark Mocha</a>,
+    <a href="https://www.tokopedia.com/senikersku/air-jordan-1-retro-low-og-travis-scott-olive-100-authentic-39?extParam=ivf%3Dfalse&src=topads" target="_blank" rel="noopener">Air Jordan 1 Retro Low OG Travis Scott Olive</a>, and
+    <a href="https://jdsports.id/product/new-balance-530-mr530ra?gad_source=1&gclid=Cj0KCQjw2a6wBhCVARIsABPeH1tfI7pUaOuSJaVZVMQboKc9VtU7AxoS8ST779IwpF_bAW356-yJ2gsaAsbyEALw_wcB" target="_blank" rel="noopener">NB 530</a>. kalau kalian pengen nyari sneakers bisa visit official storenya
+    <a href="https://www.instagram.com/807garage.id/" target="_blank" rel="noopener">807Garage</a>
+  </WelcomeItem>
+
+  
+</template>
